53f3d8a198a112100317a494e3533c8c75708356
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-10-24 11:01:58 +0200

    ✨feature: Add auth0

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 const app = express()
 const port = 3007
 
@@ -7,14 +8,22 @@ const identityRouter = require("./routes/identity");
 const savingRouter = require("./routes/saving");
 const transactionRouter = require("./routes/transaction");
 
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'http://localhost:300',
+  issuerBaseURL: `https://dev-ducb3de5dqthsoxl.us.auth0.com/`,
+});
+
 app.get('/', (_req, _res) => {
-  res.send('Hello World!')
+  _res.send('Hello World!')
 })
 
-app.use("/household", householdRouter)
-app.use("/identity", identityRouter)
-app.use("/saving", savingRouter)
-app.use("/transaction", transactionRouter)
+app.use("/household", checkJwt, householdRouter)
+app.use("/identity", checkJwt, identityRouter)
+app.use("/saving", checkJwt, savingRouter)
+app.use("/transaction", checkJwt, transactionRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
