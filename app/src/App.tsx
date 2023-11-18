bac1a3723f2ffc0448fd982673d06ddd9cecdfea
Dávid Mihalčin <mihalcindavid@gmail.com>
2023-11-18 18:38:00 +0100

    feat: structure

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Typography } from '@mui/material';
--- app/src/routes/Login.tsx
+++ app/src/routes/Login.tsx
@@ -1,12 +1,23 @@
-import { Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <Button onClick={() => loginWithRedirect()} variant={'contained'}>
-      LOGIN
-    </Button>
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      height={'100vh'}
+      flexDirection={'column'}
+      gap={10}>
+      <Typography variant={'h1'} component={'h1'} fontWeight={800}>
+        Welcome Back!
+      </Typography>
+      <Button onClick={() => loginWithRedirect()} variant={'contained'}>
+        LOGIN
+      </Button>
+    </Box>
   );
 };
 
