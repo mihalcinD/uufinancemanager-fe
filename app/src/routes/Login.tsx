52375d74b88e82b557f9351a106cb127c8061edc
J <j@gmail.com>
2023-11-29 20:00:22 +0100

    Log in screen

--- app/src/routes/Login.tsx
+++ app/src/routes/Login.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Typography } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
+import LoginIcon from '@mui/icons-material/Login';
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
@@ -10,12 +11,17 @@ const Login = () => {
       alignItems={'center'}
       height={'100vh'}
       flexDirection={'column'}
-      gap={10}>
-      <Typography variant={'h1'} component={'h1'} fontWeight={800}>
-        Welcome Back!
+      gap={10}
+      sx={{bgcolor: 'primary.dark'}}
+      >
+      <Typography variant={'h3'} component={'h3'} fontWeight={800}>
+        Welcome to the uuFinanceManager.
       </Typography>
+      <Typography variant={'h4'} component={'h4'} >
+        Please log in to continue to the application.      
+        </Typography>
       <Button onClick={() => loginWithRedirect()} variant={'contained'}>
-        LOGIN
+        LOGIN <LoginIcon/>
       </Button>
     </Box>
   );
