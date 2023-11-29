b0c0a85fbc426e0ee99fc57cb119934431cbbfe6
Dávid Mihalčin <mihalcindavid@gmail.com>
2023-11-29 21:58:44 +0100

    feat(loading): circular

--- app/src/routes/Loading.tsx
+++ app/src/routes/Loading.tsx
@@ -1,5 +1,10 @@
+import { CircularProgress, Box } from '@mui/material';
 const Loading = () => {
-  return <>AUTHENTICATION IN PROGRESS... </>;
+  return (
+    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'100vh'}>
+      <CircularProgress size={150} />
+    </Box>
+  );
 };
 
 export default Loading;
