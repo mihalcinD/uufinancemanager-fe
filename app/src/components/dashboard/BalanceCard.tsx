86549fbe6cf632bc9ba26794fd04f6fb3b231ff7
Dávid Mihalčin <mihalcindavid@gmail.com>
2023-12-11 08:11:17 +0100

    feat(dashboard): context

---
+++ app/src/components/dashboard/BalanceCard.tsx
@@ -0,0 +1,5 @@
+const BalanceCard = () => {
+  return <div></div>;
+};
+
+export default BalanceCard;
--- app/src/routes/Dashboard.tsx
+++ app/src/routes/Dashboard.tsx
@@ -1,36 +1,67 @@
 import { useParams } from 'react-router-dom';
 import Grid from '@mui/material/Unstable_Grid2';
-import { Box, Paper, useTheme } from '@mui/material';
+import { Box, Paper, Skeleton, useTheme } from '@mui/material';
 import ContentWrapper from '../components/ContentWrapper.tsx';
+import { useHouseholdsContext } from '../context/HouseholdsContext.tsx';
 
 const GRID_SPACING = 2.5;
 const Dashboard = () => {
   const { id } = useParams<{ id: string }>();
   const theme = useTheme();
-  console.log(theme.palette.background.paper);
+  const { households, isLoading } = useHouseholdsContext();
+
   return (
     <ContentWrapper>
       <Grid container rowSpacing={GRID_SPACING} columnSpacing={GRID_SPACING}>
         <Grid xs={12} md={6}>
-          <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          {isLoading ? (
+            <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: 300 }} />
+          ) : (
+            <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          )}
         </Grid>
         <Grid xs={12} md={6}>
           <Box height={300} display={'flex'} flex={1} flexDirection={'column'} gap={GRID_SPACING}>
-            <Paper elevation={8} sx={{ display: 'flex', flex: 4 }} />
+            {isLoading ? (
+              <Skeleton variant="rounded" sx={{ display: 'flex', flex: 4 }} />
+            ) : (
+              <Paper elevation={8} sx={{ display: 'flex', flex: 4 }} />
+            )}
             <Box display={'flex'} flex={1} gap={GRID_SPACING}>
-              <Paper elevation={8} sx={{ display: 'flex', flex: 1 }} />
-              <Paper elevation={8} sx={{ display: 'flex', flex: 1 }} />
+              {isLoading ? (
+                <>
+                  <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: '100%' }} />
+                  <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: '100%' }} />
+                </>
+              ) : (
+                <>
+                  <Paper elevation={8} sx={{ display: 'flex', flex: 1 }} />
+                  <Paper elevation={8} sx={{ display: 'flex', flex: 1 }} />
+                </>
+              )}
             </Box>
           </Box>
         </Grid>
         <Grid xs={12} md={6} lg={4}>
-          <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          {isLoading ? (
+            <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: 300 }} />
+          ) : (
+            <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          )}
         </Grid>
         <Grid xs={12} md={6} lg={4}>
-          <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          {isLoading ? (
+            <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: 300 }} />
+          ) : (
+            <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          )}
         </Grid>
         <Grid xs={12} md={6} lg={4}>
-          <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          {isLoading ? (
+            <Skeleton variant="rounded" sx={{ display: 'flex', flex: 1, height: 300 }} />
+          ) : (
+            <Paper elevation={8} sx={{ display: 'flex', flex: 1, height: 300 }} />
+          )}
         </Grid>
       </Grid>
     </ContentWrapper>
