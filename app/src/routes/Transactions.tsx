ae69ea495820b803b3733b43520bcee98c0139d8
Dávid Mihalčin <mihalcindavid@gmail.com>
2024-01-17 14:59:02 +0100

    feat(addTransaction): modal

--- app/src/routes/Transactions.tsx
+++ app/src/routes/Transactions.tsx
@@ -1,15 +1,19 @@
 import ContentWrapper from '../components/ContentWrapper.tsx';
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { useTransactionsContext } from '../context/TransactionsContext.tsx';
 import TransactionsList from '../components/transactions/TransactionsList.tsx';
+import AddTransactionButton from '../components/transactions/AddTransactionButton.tsx';
 
 const Transactions = () => {
   const { transactions } = useTransactionsContext();
   return (
     <ContentWrapper>
-      <Typography variant={'h2'} fontWeight={900} p={2}>
-        Transactions
-      </Typography>
+      <Stack p={2} direction="row" gap={2} justifyContent={'space-between'} flexWrap={'wrap'} alignItems={'center'}>
+        <Typography variant={'h2'} fontWeight={900} p={2}>
+          Transactions
+        </Typography>
+        <AddTransactionButton />
+      </Stack>
       <TransactionsList transactions={transactions} />
     </ContentWrapper>
   );
